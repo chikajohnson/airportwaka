7e9f7deaeac8a205dd40910ce7a3b99a7efc94a6
chikajohnson <tarboshi@yahoo.com>
2018-08-17 06:28:39 +0100

    configured mapbox

--- AirportWaka/Pages/Index.cshtml.cs
+++ AirportWaka/Pages/Index.cshtml.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AirportWaka.Pages
 {
     public class IndexModel : PageModel
     {
+        public string MapboxAccessToken { get; set; }
+        public IndexModel(IConfiguration configuration)
+        {
+            MapboxAccessToken = configuration["Mapbox:AccessToken"];
+        }
         public void OnGet()
         {
 
